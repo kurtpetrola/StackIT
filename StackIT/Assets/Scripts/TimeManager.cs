447a127e953d77ce43559d4cda69c737fca930ce
Kurt Guen Petrola <94431169+kurtpetrola@users.noreply.github.com>
2024-03-07 02:09:09 +0800

    Miscellaneous Chores
    Add time manager and update some packages

---
+++ StackIT/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    private static TimeManager instance;
+
+
+    public Timer timer;
+
+    private void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void StartTimer()
+    {
+        if (timer != null)
+        {
+            timer.StartTimer();
+        }
+    }
+
+
+    public void StopTimer()
+    {
+        if (timer != null)
+        {
+            timer.PauseTimer();
+        }
+    }
+
+
+    public void ResetTimer()
+    {
+        if (timer != null)
+        {
+            timer.ResetTimer();
+        }
+    }
+}
