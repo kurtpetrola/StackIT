0075cc0f8784546e4f3330866df346d398ecbc3a
Kurt Petrola <kurtguenpetrola2.0@gmail.com>
2024-03-12 04:24:43 +0800

    Objectives Timer
    Add more time to read objectives/challenges

--- StackIT/Assets/Scripts/StartPanel.cs
+++ StackIT/Assets/Scripts/StartPanel.cs
@@ -5,7 +5,7 @@ public class StartPanel : MonoBehaviour
 {
     public GameObject panel;
     public Text countdownText;
-    private float countdown = 6f;
+    private float countdown = 11f;
     public bool panelClosed = false;
 
     private GameplayController gameplayController;
