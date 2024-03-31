76d985a374ec5dd3877a58bbbd007655af96ff90
Kurt Petrola <kurtguenpetrola2.0@gmail.com>
2024-03-31 09:09:38 +0800

    CFXR Package
    Package for effects in our game

--- StackIT/Assets/Scripts/TimeManager.cs
+++ StackIT/Assets/Scripts/TimeManager.cs
@@ -4,24 +4,8 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
-    private static TimeManager instance;
-
 
     public Timer timer;
-
-    private void Awake()
-    {
-
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
 
 
     public void StartTimer()
